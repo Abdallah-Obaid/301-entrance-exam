c0433257dd08cf5849ea241124eff4d2f303a2e4
Abdallah <eng.abdallahobaid@gmail.com>
2020-03-16 12:56:01 +0200

    By Abdallah Obaid

---
+++ js/q2.js
@@ -0,0 +1,11 @@
+'use strict'
+var fStar='';
+var Star= '*';
+var numberOfRows=5;
+function greaterthan(){
+    for (var i = 0; i < numberOfRows; i++) {
+        fStar=fStar+Star
+        console.log(fStar);
+    }return;  
+    } ;
+    greaterthan();
